d55597ca7dfc0fadba2b06d22bfe9717e19b9965
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-11 18:46:30 +0530

    Arithmetic operators in python,Also Floor Division // and Exponential operator **

---
+++ ArithmeticOp.py
@@ -0,0 +1,25 @@
+#with int data type
+a=10
+b=2
+print("a+b=",a+b)
+print("a-b=",a-b)
+print("a*b=",a*b)
+print("a/b=",a/b)# / operator always return float
+print("a%b=",a%b)# % operator returns int for int/int and float for float/int
+print("a//b=",a//b)#for int data type with floor division(int//int=> returns int)
+print("a**b=",a**b)
+#for float data type with floor division(float//int=> returns float)
+c=10.2
+d=2
+print("c//d=",c//d)
+#for str data type + as concatenation and * as repetation operator
+s="Rishabh"+"3"
+print(s)
+#s="rishabh"+3
+#print(s+3) //gives error
+
+# * operator as repetation operator
+r="Rishabh"*3
+print(r)
+
+# s1="Rishabh"+'3' //gives error
