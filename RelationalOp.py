ec92afebe0dbb5223cc1bc3f77fa5292f8ac4b92
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-11 18:58:45 +0530

    Relational Operator in Python with int,str,bool data type,Also Chaining of relational operator.

---
+++ RelationalOp.py
@@ -0,0 +1,30 @@
+#Relational operator with int data type
+a=20
+b=10
+print("a>b=",a>b)
+print("a>=b=",a>=b)
+print("a<b=",a<b)
+print("a<=b=",a<=b)
+
+#Relational operator with str data type
+a='Rishabh'
+b='Jaiswal'
+print("a>b=",a>b)
+print("a>=b=",a>=b)
+print("a<b=",a<b)
+print("a<=b=",a<=b)
+
+#Relational operator with bool data type
+a=True
+b=False
+print("a>b=",a>b)
+print("a>=b=",a>=b)
+print("a<b=",a<b)
+print("a<=b=",a<=b)
+
+#chaining of Relational Operator
+
+print(10>20>30>40)
+print(10<20>30<40)
+print(10<20<30<40)
+
