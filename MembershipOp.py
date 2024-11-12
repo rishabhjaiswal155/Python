7bccaa56d58748cf85958baf0698ae1fe13aab7d
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-12 20:31:57 +0530

    Special Operator: Membership Operator (in ,not in)

---
+++ MembershipOp.py
@@ -0,0 +1,8 @@
+#Special Operator: Membership Operator(in,not in)
+list1=[10,20,30]
+print(10 in list1)
+s="Hello!! Learning python is very easy..."
+print('python' in s)
+print(" " in s)
+print('Rishabh' not in s)
+ 
