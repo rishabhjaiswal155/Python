851ebfdbe0ab34d7f3566dcba7610040b9c59c32
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-12 20:51:12 +0530

    Input Statements and TypeCast function int() in Python.

---
+++ InputStatements.py
@@ -0,0 +1,4 @@
+#Input statements and TypeCasting Function like int()
+a=int(input("Enter first Number:")) #Default input takes str so need to typeCast 
+b=int(input("Enter Second Number:"))
+print("The Sum is: ",a+b)
