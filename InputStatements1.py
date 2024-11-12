74afbbcd8d33baa152aedb08a5750a11273cc807
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-12 22:31:45 +0530

    Reading the Employee Data from Keyboard using Input function and TypeCast Function

---
+++ InputStatements1.py
@@ -0,0 +1,14 @@
+#Reading employee Data from Keyboard
+enum=int(input("Enter Employee number:"))
+ename=input("Enter Employee Name:")
+esal=float(input("Enter Employee Salary:"))
+eaddr=input("Enter Employee Address:")
+married=bool(input("Enter Employee marriage status[True|False]:"))
+print("Please Confirm the data")
+print("Employee number:",enum)
+print("Employee name:",ename)
+print("Employee Salary:",esal)
+print("Employee Address:",eaddr)
+print("Employee marriage Status:",married)
+
+
