0af487f7fd266ecf055a476be88b99a442338596
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-12 20:41:14 +0530

    Operator Precedence in Python

---
+++ OperatorPrecedence.py
@@ -0,0 +1,7 @@
+#Operator Precedence in Python
+a=10+(30*2)/40 # () has highest precedence in python
+print(a)
+b=(10+3)+2**2+3 #precendence in decreasing order is (),**,~,-(unary),*,/,%,//,+,-(binary),<<,>>,&,|,^,>,>=,<,<=,is,is not,,in,not in,assignment operator...
+print(b)
+x,y,z,p=30,20,10,5
+print((x+y)*z/p)
