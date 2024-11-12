39c8b55e0c3661d9fa640d5a7e03382f88602cea
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-12 20:46:10 +0530

    Modules and Aliasing as in python

---
+++ ModuleAndAlias.py
@@ -0,0 +1,14 @@
+#Module and Libraries
+import math
+print(math.sqrt(25))
+print(math.pi)
+
+#Aliasing as
+import math as m
+print(m.sqrt(81))
+print(m.pi)
+
+#
+from math import sqrt,pi
+print(sqrt(100))
+print(pi)
