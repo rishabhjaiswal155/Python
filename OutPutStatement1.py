0a975e9484b4f3566adb4d86cc6e8caef34607d3
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-13 17:11:39 +0530

    form-3 of print() with variable number of args,sep,end attribute.

--- OutPutStatement1.py
+++ OutPutStatement1.py
@@ -7,3 +7,14 @@ print("Rishabh"+"Jaiswal") #'+' with both str args treated as concatenation
 print("Rishabh"*3) #'*' as repeatation operator having one str and other int arg
 print("Rishabh","Jaiswal") #',' treat as space
 
+#form-3(with variable number of args)
+a,b,c,d=10,20,30,"Rishabh"
+print("The values are:",a,b,c,d)
+
+#sep,end atttributes of print()
+print(a,b,c,d)#default seperator is space
+print(a,b,c,d,sep=':')#sep attribute
+print("Rishabh",end='....')#end attribute
+print("Jaiswal")
+
+
