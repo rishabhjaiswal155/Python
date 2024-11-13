99c981e8c763328c24913c18e0e0e0c0439a1011
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-14 02:50:16 +0530

    Program to count the number of characters from String using for iterative Statement.

---
+++ CountCharacters.py
@@ -0,0 +1,9 @@
+#Flow Control:Iterative Statements for,while
+#Program to find the number of characters in given str without using len()
+s=input("Enter the String:")
+count=0
+for x in s:
+	count+=1
+print("The number of character in ",s," is: ",count)
+
+	
