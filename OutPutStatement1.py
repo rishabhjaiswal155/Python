1b0dc76396b3e4ea8c6093b4bcc622a665e32d67
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-13 17:16:36 +0530

    print() with Object as arguments

--- OutPutStatement1.py
+++ OutPutStatement1.py
@@ -6,15 +6,26 @@ print("Rishabh\nJaiswal")#'\n' like escape operators can be used
 print("Rishabh"+"Jaiswal") #'+' with both str args treated as concatenation
 print("Rishabh"*3) #'*' as repeatation operator having one str and other int arg
 print("Rishabh","Jaiswal") #',' treat as space
-
+print("-----------------------------")
 #form-3(with variable number of args)
 a,b,c,d=10,20,30,"Rishabh"
 print("The values are:",a,b,c,d)
-
+print("---------------------------------")
 #sep,end atttributes of print()
 print(a,b,c,d)#default seperator is space
 print(a,b,c,d,sep=':')#sep attribute
 print("Rishabh",end='....')#end attribute
 print("Jaiswal")
+print("---------------------------------------")
+
+#print() with Object as argument
+list=[10,20,30,40]
+tuple=(100,200,300,400,'Rishabh')
+set={1,2,3,4,'Rishabh'}
+print(list,tuple,set)
+
+print("---------------------------------")
+
+
 
 
