1ca23ae93fe53f7de12723e1f81033f32e0f7f20
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-14 02:36:49 +0530

    Program to find whether the number is between the range of 1-100.

---
+++ NumberinRange.py
@@ -0,0 +1,7 @@
+#Program to find whether the number is in between the range of 1-100
+n1=int(input("Enter the number:"))
+if n1>=1 and n1<=100:
+	print("The Number ",n1," is in range of 1-100")
+else:
+	print("The Number ",n1," is not in range of 1-100")
+	
