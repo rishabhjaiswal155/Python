14c334aaaf0b935e5271ced9b62c1eb639ce2fb3
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-14 03:06:14 +0530

    Program to display odd numbers between 0-20 using for loop and range(1,21,2) function

---
+++ OddNumber.py
@@ -0,0 +1,9 @@
+#Program to display Odd numbers between 0-20
+for x in range(21):
+	if x%2!=0:
+		print(x)
+print("----------------------------------------------")    
+# using range only i.e with out if
+for x in range(1,21,2):
+    print(x)
+    
