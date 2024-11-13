9a2cbc3317faffec1e132baecc5d73dd9d473231
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-14 02:43:42 +0530

    Program to enter digit from 0-9 and print it in English word form using if,elif,else.

---
+++ DigitToWord.py
@@ -0,0 +1,25 @@
+#Program to enter digit from keyboard between 0-9 and print it in its English word form.
+n=int(input("Enter the digit between 0-9:"))
+if n==0:
+	print("ZERO")
+elif n==1:
+	print("ONE")
+elif n==2:
+	print("TWO")
+elif n==3:
+	print("THREE")
+elif n==4:
+	print("FOUR")
+elif n==5:
+	print("FIVE")
+elif n==6:
+	print("SIX")
+elif n==7:
+	print("SEVEN")
+elif n==8:
+	print("EIGHT")
+elif n==9:
+	print("NINE")
+else:
+	print("The digit:",n," is not in between 0-9")
+	
