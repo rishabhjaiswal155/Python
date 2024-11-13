3bc74a985adfb19ec3a03553358f9f962b1812b3
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-14 02:54:45 +0530

    program to print character and its index of Str taken from keyboard using for loop.

---
+++ PrintCharacterandIndex.py
@@ -0,0 +1,6 @@
+#Program to read Str from Keyboard and print each character and its index.
+s=input("Enter the String:")
+i=0
+for x in s:
+	print("The character is:",x," and its index is:",i)
+	i+=1
