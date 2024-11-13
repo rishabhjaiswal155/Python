29f38f21a4c3de77ea9700e585349d54e451d8e1
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-13 15:03:36 +0530

    1.finding the number of Command line arguments passed using len() function. 2.Printing command line argumnts one by one using for loop.

--- cmdLineArgs.py
+++ cmdLineArgs.py
@@ -2,3 +2,14 @@
 from sys import argv
 print(type(argv))
 print(argv[1:])
+
+#To find the number of Command line arguments passed
+from sys import argv
+args=argv[1:]
+print("Length of argv:",len(args))
+
+#Printing command line argumnts one by one
+from sys import argv
+print("Printing command line arguments one by one")
+for x in argv[1:]:
+  print(x)
