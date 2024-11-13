20c86c5e0edb447b2c661ef992a09d78df2d8f74
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-14 02:31:56 +0530

    Program to find whether the number is even or odd.

---
+++ EvenOdd.py
@@ -0,0 +1,7 @@
+#Program to take input number from keyboard and find the number is even or odd.
+n1=int(input("Enter the number:"))
+if n1%2==0:
+	print(n1,":is Even number")
+else:
+	print(n1,":is odd number")
+	
