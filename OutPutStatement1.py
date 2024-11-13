9d5636681f75e1d9fed20cebeb96d0aad1ab50b5
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-13 17:04:12 +0530

    Output Statement print() with form-1 and form-2 using +,*,escape operators,comma etc

---
+++ OutPutStatement1.py
@@ -0,0 +1,9 @@
+#form-1(without any args used for new line character) and form-2(with str as arg) of output statements
+print("Hello,Rishabh")
+print()
+print("How are You!!")
+print("Rishabh\nJaiswal")#'\n' like escape operators can be used
+print("Rishabh"+"Jaiswal") #'+' with both str args treated as concatenation
+print("Rishabh"*3) #'*' as repeatation operator having one str and other int arg
+print("Rishabh","Jaiswal") #',' treat as space
+
