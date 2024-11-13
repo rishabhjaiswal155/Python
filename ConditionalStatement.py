eec293ac5ff23d12ebcbb17e27c3de6f3dd7d284
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-13 18:09:11 +0530

    Flow Control statement like conditional/selection statement(if,if elif,if else,if elif else)

---
+++ ConditionalStatement.py
@@ -0,0 +1,24 @@
+#Flow Control statement: Conditional Statement if
+name="Rishabh"
+if name=="Rishabh":
+	print("Hii %s"%name)
+print("How are You!!!")
+
+#if else
+name="Vishal"
+if name=="Vishal":
+	print("Hello %s"%name)
+else:
+	print("Hello Guest")
+print("How are you")
+
+#if elif else
+name="Amol"
+if name=="Amol":
+	print("Priyanka")
+elif name=="Shreyash":
+	print("Surabhi")
+else:
+	print("Rishabh")
+print("Jaiswal")
+
