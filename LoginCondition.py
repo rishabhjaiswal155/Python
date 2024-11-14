918dca7e318c0e3c58d20f081ef288a456d88918
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-14 18:08:27 +0530

    Program to check whether userName and password is valid,otherwise asked to re enter.

---
+++ LoginCondition.py
@@ -0,0 +1,8 @@
+#Program to take userName and password as input and check if it is valid then print Login Successfull ,otherwise ask to re enter.
+userName=input("Enter userName:")
+password=input("Enter password:")
+while (userName!="Rishabh") | (password!="python"):
+	userName=input("Enter userName:")
+	password=input("Enter password:")
+print("login sucessfull!!!")
+
