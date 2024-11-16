5993cbc29f527d2f282108276e53985ae1ced736
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-16 15:33:41 +0530

    str data type,its representation in single,double and tripe quotes,Also accesssing it using index,slice operator.

---
+++ strDataType.py
@@ -0,0 +1,12 @@
+#str data type 
+s='Rishabh' #reperesentation in ' '
+s1="Jaiswal"  #reperesentation in " "
+s2='''Rishabh   
+Jaiswal'''
+#reperesentation in ''' ''' or """ """ single or double triple quotes for multi line str
+
+print(s,s1,s2)
+print(s[0],s[-7],s[-2],s[6]) #Accessing str using positive and Negative index
+print(s[0:7]) # Accessing using slice operator
+ 
+ 
