311ae93aa72bdad30ea6b036c675bc60ea4784fe
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-17 21:48:33 +0530

    program to take input string and reverse it as per different requirement using Functions in str and while loop.

--- Stringreversewhile.py
+++ Stringreversewhile.py
@@ -6,3 +6,24 @@ while i>=0:
 	output=output+s[i]
 	i-=1
 print(output)
+
+#Program to take input as "Rishabh Full Stack Developer" and print output as "Developer Stack Full Rishabh"
+input="Rishabh Full Stack Developer"
+print("input:",input)
+l=input.split()
+l1=[]
+i=len(l)-1
+while i>=0:
+    l1.append(l[i])
+    i-=1
+print("output:",' '.join(l1))
+
+#Program to take input as "Rishabh Full Stack Developer" and print output as "hbahsiR lluF kcatS repoleveD".
+input="Rishabh Full Stack Developer"
+print("input:",input)
+l=input.split()
+l1=[]
+for x in l:
+    l1.append(x[::-1])
+output=' '.join(l1)
+print("output:",output)
