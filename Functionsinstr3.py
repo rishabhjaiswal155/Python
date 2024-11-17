c7d018aaa670344cd092a49bdd60fc6351df0fc6
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-17 20:45:25 +0530

    Functions in str like isalnum(),isalpha(),isupper(),islower(),isdigit(),istitle(),isspace().

---
+++ Functionsinstr3.py
@@ -0,0 +1,14 @@
+print('Abcd123'.isalnum())#True
+print('ABC'.isalnum())#False
+print('abc'.isalpha())#True
+print('Abc123'.isalpha())#False
+print('1234'.isdigit())#True
+print('Abc1234'.isdigit())#False
+print('abc123'.islower())#True
+print('1234A'.islower())#False
+print('A123'.isupper())#True
+print('123'.isupper())#False
+print('Learning Python Is Very Easy'.istitle())#True
+print('Learning java Is very Difficult'.istitle())#False
+print(' '.isspace())#True
+print('Learnng python is cool'.isspace())#False
