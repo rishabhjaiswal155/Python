dd3b39591a79fdc2d9fe30f2be0119384b14d27e
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-17 07:02:31 +0530

    Functions like upper(),lower(),swapcase(),title(),capitalize(),startswith(),endswith().

---
+++ Functionsinstr2.py
@@ -0,0 +1,10 @@
+#Functions like upper(),lower(),swapcase(),title(),capitalize(),startswith(),endswith()
+
+s="Learning python is very easy,because of its concise code."
+print("upper():",s.upper())
+print("lower():",s.lower())
+print("swapcase():",s.swapcase())
+print("title():",s.title())
+print("capitalize():",s.capitalize())
+print("startswith():",s.startswith("Learning"))
+print("endswith():",s.endswith("Code"))
