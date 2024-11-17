4f257522c1935f9c8df76f4c2876baf4913b0993
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-17 06:28:38 +0530

    Program to count number of Occurences of substring in main string using count()

---
+++ CountStr.py
@@ -0,0 +1,5 @@
+#Program to count number of Occurences of substring in main string using count().
+s=input("Enter some String:")
+sub=input("Enter substring to search:")
+n=s.count(sub)
+print("Number of Occurences of substring",sub,"in main String",s,"are:",n)
