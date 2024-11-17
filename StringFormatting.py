c46e29a857a5a5f414cce7f1c23d126063be92a3
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-17 21:02:24 +0530

    String formatting using {} replacement operator and .format().

---
+++ StringFormatting.py
@@ -0,0 +1,7 @@
+#Formatting the String
+name="Rishabh"
+age=28
+salary=52000
+print("{}'s age is {} and his salary is {}".format(name,age,salary))
+print("{0}'s age is {1} and his salary is {2}".format(name,age,salary))
+print("{z}'s age is {x} and his salary is {y}".format(z=name,x=age,y=salary))
