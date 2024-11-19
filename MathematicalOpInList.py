fd8f0d0d3c872458d33130ccfbffe003320cfbc2
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-19 14:56:00 +0530

    Mathematical Operators like + used for concatenation and * used for repeatation of List.

---
+++ MathematicalOpInList.py
@@ -0,0 +1,6 @@
+#Mathematical Operators like + used for concatenation and * used for repeatation of List.
+l1=[10,20,30,40,50,60]
+l2=['Rishabh','Jaiswal','IACSD','SGGS']
+l3=l1+l2 #Here + valid only it both Operarands are List onject
+print(l3)
+print(l1*3) #Here * valid only if one operand is list and other is Int
