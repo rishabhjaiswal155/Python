3cdb4fc192c8c1a0714de858fec230e50602c5be
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-19 14:43:54 +0530

    Manipulating the order of elements in List using reverse(),sort(),sort(reverse=True).

---
+++ ManipulateOrderList.py
@@ -0,0 +1,20 @@
+#Ordering elements of the List
+l=[10,20,30,40,'Rishabh']
+l.reverse() #reverse() to print the list in reverse order
+print(l)
+
+l=[10,25,65,14,98,12,45,67,32,3021]
+l.sort() #to sort the homogenous items in the list in natural sorting order i.e Ascending
+print(l)
+
+l=['Rishabh','Raj','Rama','Ashok','Vithal']
+l.sort() #to sort the homogenous items in the list in natural sorting order i.e Ascending 
+print(l)
+
+l=[50,20,45,98,21,47,4789,30,3,2,1]
+l.sort(reverse=True) #to sort the homogenous items in the list in reverse sorting order i.e Descending
+print(l)
+
+l=['Abhilasha','Abhishek','Sarfaraz','vishal']
+l.sort(reverse=True) #to sort the homogenous items in the list in reverse sorting order i.e Descending 
+print(l)
