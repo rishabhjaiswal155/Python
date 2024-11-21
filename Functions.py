ec07702599f5e1dd0c461885b05d62e7113a21fd
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-21 09:38:07 +0530

    Functions in python and keywords like def,return.

---
+++ Functions.py
@@ -0,0 +1,20 @@
+#Functions in Python
+def wish():
+	print("Good Morning")
+wish()
+
+def evenodd(n):
+	if n%2==0:
+		print(n,"is Even")
+	else:
+		print(n,"is odd")
+evenodd(9)
+evenodd(15)
+evenodd(22)
+
+def add(a,b):
+	return a+b
+c=add(15,12)
+print("The sum of two numbers is:",c)
+
+
