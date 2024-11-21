41ca5991dd3e52ffe2f6bfbb55f6bf433486c09b
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-21 06:08:21 +0530

    Program to accept dictionary from keyboard and print the sum of values using sum().

---
+++ Dict1.py
@@ -0,0 +1,5 @@
+#Program to accept dictionary from keyboard and print the sum of values using sum().
+
+d=eval(input("Enter the dictionary items:"))
+print(d)
+print("The sum of values of dictionary items is:",sum(d.values()))
