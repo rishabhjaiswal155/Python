244e4294970cbb2cdcc87e6479e0adf4a0caab68
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-28 04:27:38 +0530

    math module in python and its various methods.

---
+++ MathModule.py
@@ -0,0 +1,9 @@
+#math module
+import math
+from math import *
+print("the members available in math module are:",dir(math))
+print(sqrt(4))
+print(floor(11.2))
+print(ceil(4.5))
+print(fabs(-11.5))
+print(cbrt(27))
