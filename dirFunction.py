fb61b3f9c8fc5e4323dfaa2448b6ba7f1c23c5f3
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-11-28 04:07:44 +0530

    dir() function to find all the members of current module and dir(modulename) to find all the members of specified module.

---
+++ dirFunction.py
@@ -0,0 +1,10 @@
+#dir() to find all the members of current module
+#dir(modulename) to find all the members of specified module
+
+import Module1
+x=10
+y=25
+def f1():
+	print("Hello")
+print(dir()) #finding all the members of current module
+print(dir(Module1)) #finding all the members of specified module i.e Module1
